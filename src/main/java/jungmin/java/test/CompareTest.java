0a54e7aa9b8d93160fb5a8e3e0faf72a438c54c6
duck <jungmin5.hur@gmail.com>
2019-05-24 22:51:35 +0900

    test codes

---
+++ src/main/java/jungmin/java/test/CompareTest.java
@@ -0,0 +1,58 @@
+package jungmin.java.test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class CompareTest {
+    public static void main(String args[]) {
+        Point p1 = new Point(1,3,3);
+        Point p2 = new Point(2,3,5);
+        Point p3 = new Point(3,1,2);
+        Point p4 = new Point(4,1,6);
+        Point p5 = new Point(5,1,1);
+        Point p6 = new Point(6,3,2);
+
+        List<Point> points = new ArrayList<>();
+        points.add(p1);
+        points.add(p2);
+        points.add(p3);
+        points.add(p4);
+        points.add(p5);
+        points.add(p6);
+
+        System.out.println("before sort....");
+        for(int i=0; i<6; i++){
+            System.out.println(points.get(i).id);
+        }
+
+        Collections.sort(points);
+        System.out.println("after sort....");
+        for(int i=0; i<6; i++){
+            System.out.println(points.get(i).id);
+        }
+    }
+
+    static class Point implements Comparable<Point>{
+        int id;
+        int x, y;
+
+        Point(int id, int x, int y){
+            this.id = id;
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public int compareTo(Point o) {
+            if(this.x > o.x) { //x가 작은 것이 우선
+                return 1;
+            } else if(this.x == o.x) {
+                if(this.y < o.y) { //x가 같다면 y가 큰 것이 우선
+                    return 1;
+                }
+            }
+            return -1;
+        }
+    }
+}
